1ac78576337dd3867cc1a455b1fa746fe9316c2b
tauheed-12 <mohammadtauheed160@gmail.com>
2024-06-09 02:26:49 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import SearchPage from "./component/SearchPage";
 import BookshelfPage from "./component/BookshelfPage";
 
